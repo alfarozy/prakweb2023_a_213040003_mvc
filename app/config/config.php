b95a0c1e4cd353709d61d915b295f4062907c51b
alfarozy.an <mr.alfarozy.a.n@gmail.com>
2023-10-22 16:38:28 +0700

    MVC - Simple Framework

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/~koji/prakweb_2023_A_213040003/mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'sandwich');
+define('DB_NAME', 'project_phpmvc');
